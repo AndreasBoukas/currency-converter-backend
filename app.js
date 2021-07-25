8dd2ae32167f6c32eba7712af1570b74a52edf73
AndreasBoukas <andrew.boukas@gmail.com>
2021-07-25 18:20:30 +0300

    added port env

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.robql.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
